37b6bd94e7d80e60c3a25b3dc2ac2341a6cfa202
smridhi katyal <smridhi.katyal245@gmail.com>
2024-04-01 04:03:29 +0530

    First Commit

---
+++ build/web/flutter_service_worker.js
@@ -0,0 +1,217 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"assets/AssetManifest.bin": "9804796e92b9be2d498c1e665adeef18",
+"assets/AssetManifest.json": "f1adba8c8783c0cf5f828d1b4b081dd3",
+"assets/assets/dialog_flow_auth.json": "a5288f8eddc56a7c3c4013e2b065530a",
+"assets/assets/images/1%2520(2).jpg": "98e275b18dfe04b7cad39ff745115ab0",
+"assets/assets/images/2.jpg": "ac034d6a41d5bb6b1fb4556392db59d3",
+"assets/assets/images/5.jpg": "5c5ea7494c8fe0c7eecaaaeeaa536951",
+"assets/assets/images/6.jpg": "fc69ec1463d662fde61d13bc609c2dfb",
+"assets/assets/images/background.jpg": "ee9869fbfaff434f823a2385250416b2",
+"assets/assets/images/image1.jpg": "c83f15da72d7893f0981beae6e409851",
+"assets/assets/images/image2.jpg": "f193cf42a567d2975dcf3a84fa8e539a",
+"assets/assets/images/image3.jpg": "d7392412c6bea2588ca440b78325e787",
+"assets/assets/images/image4.jpg": "c3d3908ad9cd739a14568e3780fe89bd",
+"assets/assets/images/imagee.jpg": "ff0f527352cd40e3910b8c5c9c2ff4f5",
+"assets/assets/images/sliderbg.jpg": "88edc8429d739fea1124ece1cefdffbe",
+"assets/assets/Lottie/Animation%2520-%25201711305245092.json": "61132822f018f5386d13efe3e0db3825",
+"assets/assets/Lottie/Animation%2520-%25201711659056289.json": "12023ab8f3dabac7dddde1d248edc7dd",
+"assets/assets/Lottie/Animation%2520-%25201711659177872.json": "d058cf29aa46552bea983c1f17b1120c",
+"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
+"assets/fonts/MaterialIcons-Regular.otf": "57a9b37d93b7f8f997d1f3402e601e2c",
+"assets/NOTICES": "960e8b8a9e635bc2e8a294126c252ab0",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "90befbec7bfc3e9748412e5088df93d4",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "60622e6391fe560cb9d2b1c5863fb805",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "cb805a1a9880422e6640dc1a9f51a4b8",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"canvaskit/canvaskit.js": "3bd93dfe6f74ec4261f82b4d4c2c63dc",
+"canvaskit/canvaskit.wasm": "df158fb454a09fd4a5a47ab325bdbd23",
+"canvaskit/chromium/canvaskit.js": "2829bb10a7eb9912e12b452dfd671141",
+"canvaskit/chromium/canvaskit.wasm": "ee3260e5702e374455ba146f6a500d14",
+"canvaskit/skwasm.js": "5256dd3e40ec9fe1fc9faa51a116bcfd",
+"canvaskit/skwasm.wasm": "4ca73baf420612f1657d5b2be4b7467d",
+"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "a96e4cac3d2da39d86bf871613180e7b",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "194b7183815e3ad19a890df8128de6b1",
+"/": "194b7183815e3ad19a890df8128de6b1",
+"main.dart.js": "9988eef9730c99feec0de756dac27d96",
+"manifest.json": "e76cd82539f143bfabb1a6ba8d19e6a2",
+"version.json": "73be94451399d6d89ceb7499d04660f1"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\fluttervscode\\projects\\chatbot" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "PROJECT_DIR=C:\\fluttervscode\\projects\\chatbot"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\fluttervscode\\projects\\chatbot\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\fluttervscode\\projects\\chatbot"
+  "FLUTTER_TARGET=C:\\fluttervscode\\projects\\chatbot\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mYmM2ZjRhNTQwNDdlOTVhYmMwZmVmNzA3N2YyN2RhOWY4ZTU1Yjc3Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\fluttervscode\\projects\\chatbot\\.dart_tool\\package_config.json"
+)
